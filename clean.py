df490dece2b8153985a25ffbd5a55cb8190bfa44
Paul Craciunoiu <paul@uplift.ltd>
2023-03-08 16:31:58 -0700

    MMS and SMS deduping (#1)
    * Add poetry, dependencies. SMS/MMS deduping
    
    * Generate indented XML.

--- clean.py
+++ clean.py
@@ -1,32 +1,42 @@
 import sqlite3
+
 try:
     from lxml.etree import ETCompatXMLParser
+
     custom_parser = ETCompatXMLParser(huge_tree=True, recover=True)
 except ImportError:
     custom_parser = None
+
 
 try:
     import xml.etree.cElementTree as XML
 except ImportError:
     import xml.etree.ElementTree as XML
-import logging as log
-from datetime import datetime
+
 import argparse
-import glob, fnmatch, os, re
+import fnmatch
+import glob
+import logging as log
+import os
+import re
+from datetime import datetime
+
+import phonenumbers
 
 #    TODO SMSCOUNT backup_set backup_date
 #
 
 GLOB_ID_MMS = 0
+DEFAULT_REGION = "US"
 
 
 def main(input_paths, output_path):
     try:
         time_start = datetime.now()
-        log.basicConfig(level=log.DEBUG, format='%(asctime)s %(message)s')
-        log.debug('Starting Operation...')
-        conn = sqlite3.connect('sms.db')
-        conn.execute('DELETE FROM smss')
+        log.basicConfig(level=log.DEBUG, format="%(asctime)s %(message)s")
+        log.debug("Starting Operation...")
+        conn = sqlite3.connect("sms.db")
+        conn.execute("DELETE FROM smss")
         conn.execute("DELETE FROM mmss")
         conn.execute("DELETE FROM parts")
         conn.execute("DELETE FROM addrs")
@@ -37,22 +47,28 @@ def main(input_paths, output_path):
             log.debug("Parsing XML file: ")
             tree = XML.parse(xml_filename, parser=custom_parser)
             log.debug("Done.")
-            load_into_db(conn, tree)
+            total_records = load_into_db(conn, tree)
 
-        add_sms(conn, root)
-        add_mms(conn, root)
+        print("Loaded sms.db with {} messages.".format(total_records))
+        sms_uniques = add_sms(conn, root)
+        duplicates_skipped = add_mms(conn, root, sms_uniques)
+        print("Skipped {} MMS duplicates of SMS".format(duplicates_skipped))
         conn.close()
-        #append_mms(mms_list, root)
+        # append_mms(mms_list, root)
         write_file(output_path, root)
         time_end = datetime.now()
-        log.debug('Operation completed in %d seconds.' % ((time_end - time_start).total_seconds()))
+        log.debug(
+            "Operation completed in %d seconds."
+            % ((time_end - time_start).total_seconds())
+        )
     finally:
-        if 'conn' in locals():
+        if "conn" in locals():
             conn.close()
 
 
 def write_file(output_path, root):
     tree = XML.ElementTree(root)
+    XML.ElementTree.indent(tree)
     tree.write(output_path, xml_declaration=True, encoding="UTF-8")
 
 
@@ -60,6 +76,26 @@ def append_mms(mms_list, root):
     log.debug("Adding skipped %d MMS into new XML..." % len(mms_list))
     for mms in mms_list:
         root.extend(mms)
+
+
+def format_number(address):
+    try:
+        return phonenumbers.format_number(
+            phonenumbers.parse(address, DEFAULT_REGION),
+            phonenumbers.PhoneNumberFormat.INTERNATIONAL,
+        )
+    except phonenumbers.NumberParseException:
+        log.exception("Returning address unformatted: {}".format(address))
+        return address
+
+
+def make_unique_message_key(row):
+    key = "{}-{}-{}".format(
+        format_number(row[1]),  # address
+        row[3],  # type
+        row[13],  # readable date
+    )
+    return key
 
 
 def add_sms(conn, root):
@@ -70,7 +106,9 @@ def add_sms(conn, root):
     log.debug("Attempting to write new XML for SMS count: " + str(sms_count[0]))
     # Get the rows
     cursor = conn.execute("SELECT * FROM smss ORDER BY date_sent")
+    unique_sms = set()
     for row in cursor:
+        unique_sms.add(make_unique_message_key(row))
         # newfile.write("<sms protocol=\"%s\" body=\"%s\"/>\n" % (row[0], row[5]))
         sms_element = XML.SubElement(root, "sms")
         sms_element.set("protocol", row[0])
@@ -89,9 +127,18 @@ def add_sms(conn, root):
         sms_element.set("readable_date", row[13])
         sms_element.set("contact_name", row[14])
     conn.commit()
+    return unique_sms
 
 
-def add_mms(conn, root):
+def map_mms_type(mms_type):
+    # Sent and received types for MMS -> SMS
+    if mms_type == "151":
+        return "2"
+    elif mms_type == "137":
+        return "1"
+
+
+def add_mms(conn, root, unique_sms):
     log.debug("Rewriting into optimized XML...")
     cursor = conn.execute("SELECT COUNT() FROM mmss")
     mms_count = cursor.fetchone()
@@ -99,7 +146,10 @@ def add_mms(conn, root):
     log.debug("Attempting to write new XML for MMS count: " + str(mms_count[0]))
     # Get the rows
     cursorMMS = conn.execute("SELECT * FROM mmss ORDER BY date_sent")
-    for rowMMS in cursorMMS:
+    duplicates_skipped = 0
+    for row_num, rowMMS in enumerate(cursorMMS):
+        if not row_num % 250:
+            log.debug("Progress: {}/{}...".format(row_num, mms_count[0]))
         mms_element = XML.SubElement(root, "mms")
         # retrocompatibility
         mms_element.set("text_only", rowMMS[0])
@@ -235,8 +285,10 @@ def add_mms(conn, root):
             mms_element.set("privacy_mode", privacy_mode)
         # ----- write parts and addrs
         id_mms = rowMMS[55]
-        conn2 = sqlite3.connect('sms.db')
-        cursorParts = conn2.execute("SELECT * FROM parts WHERE fk_id_mms ="+str(id_mms))
+        conn2 = sqlite3.connect("sms.db")
+        cursorParts = conn2.execute(
+            "SELECT * FROM parts WHERE fk_id_mms =" + str(id_mms)
+        )
         parts_root = XML.SubElement(mms_element, "parts")
         for rowPart in cursorParts:
             part_element = XML.SubElement(parts_root, "part")
@@ -262,16 +314,32 @@ def add_mms(conn, root):
             else:
                 part_element.set("data", "null")
 
-        cursorAddrs = conn2.execute("SELECT * FROM addrs WHERE fk_id_mms ="+str(id_mms))
+        cursorAddrs = conn2.execute(
+            "SELECT * FROM addrs WHERE fk_id_mms =" + str(id_mms)
+        )
         addrs_root = XML.SubElement(mms_element, "addrs")
         for rowAddr in cursorAddrs:
             addr_element = XML.SubElement(addrs_root, "addr")
             addr_element.set("address", rowAddr[0])
             addr_element.set("type", rowAddr[1])
             addr_element.set("charset", rowAddr[2])
-        if 'conn2' in locals():
+            address_formatted = format_number(rowAddr[0])
+            unique_key = "{}-{}-{}".format(
+                address_formatted,  # address
+                map_mms_type(rowAddr[1]),  # type
+                rowMMS[51],  # readable_date
+            )
+            if unique_key in unique_sms:
+                log.debug("Found duplicate key {}.".format(unique_key))
+                duplicates_skipped += 1
+                root.remove(mms_element)
+                break
+
+        if "conn2" in locals():
             conn2.close()
+
     conn.commit()
+    return duplicates_skipped
 
 
 def insert_default(conn, sql, vals, child):
@@ -283,67 +351,86 @@ def insert_default(conn, sql, vals, child):
         # log.info("\tException: " + e.message)
         return False
     except sqlite3.OperationalError as e:
-        log.info("Skipping: Found OperationalError when processing child (%s): %s" % (child.tag, str(child)))
+        log.info(
+            "Skipping: Found OperationalError when processing child (%s): %s"
+            % (child.tag, str(child))
+        )
         log.info("\tException: " + e.message)
         return False
     return True
 
 
 def insert_sms(conn, child):
-    columns = ', '.join(child.attrib.keys())
-    placeholders = ', '.join('?' * len(child.attrib))
-    sql = 'INSERT INTO smss ({}) VALUES ({})'.format(columns, placeholders)
-    vals = child.attrib.values()
+    columns = list(child.attrib.keys())
+    vals = list(child.attrib.values())
+    if "sub_id" in columns:
+        sub_id_index = columns.index("sub_id")
+        vals.pop(sub_id_index)
+        columns.pop(sub_id_index)
+        columns = ", ".join(columns)
+    placeholders = ", ".join("?" * len(vals))
+    sql = "INSERT INTO smss ({}) VALUES ({})".format(columns, placeholders)
     return insert_default(conn, sql, vals, child)
 
 
-def mms_compatibility(child, columns):
+def mms_compatibility(attribs, columns):
     oppo_counter = columns.count("oppo")
-    columns = re.sub('(oppo_[a-z_]+,)', '', columns)
-    log.debug("columns %s" % columns)
-    placeholder_counter = (len(child.attrib)+1-oppo_counter)
-    placeholders = ', '.join('?' * placeholder_counter)
-    sql = 'INSERT INTO mmss ({}) VALUES ({})'.format(columns, placeholders)
+    columns = re.sub("(oppo_[a-z_]+,)", "", columns)
+    # log.debug("columns %s" % columns)
+    placeholder_counter = len(attribs) + 1 - oppo_counter
+    placeholders = ", ".join("?" * placeholder_counter)
+    sql = "INSERT INTO mmss ({}) VALUES ({})".format(columns, placeholders)
     return sql
 
+
 def insert_mms(conn, child):
-    columns = 'id ,'+', '.join(child.attrib.keys())
-    sql = mms_compatibility(child, columns)
-    #id_mms = conn.execute('SELECT IFNULL(MAX(id), 0) + 1 FROM mmss')
+    attribs = dict(child.attrib.items())
+    sequence_time = attribs.pop("sequence_time", None)
+    date = attribs.get("date")
+    if sequence_time and sequence_time != date:
+        log.debug("found sequence_time != date for child: " % child)
+    # TODO: should we handle _id?
+    _id = attribs.pop("_id", None)
+    star_status = attribs.pop("star_status", None)
+    if star_status and star_status != "null":
+        log.debug("found unhandled star_status value for child: " % child)
+    columns = "id ," + ", ".join(attribs.keys())
+    sql = mms_compatibility(attribs, columns)
+    # id_mms = conn.execute('SELECT IFNULL(MAX(id), 0) + 1 FROM mmss')
     global GLOB_ID_MMS
     id_mms = GLOB_ID_MMS
-    vals = child.attrib.values()
+    vals = list(attribs.values())
     vals.insert(0, id_mms)
     # ---------- PARTS AND ADDRS ---------------
     rst = insert_default(conn, sql, vals, child)
 
-    GLOB_ID_MMS = GLOB_ID_MMS+1
+    GLOB_ID_MMS = GLOB_ID_MMS + 1
     if rst:
-        child_parts = child.find('parts')
-        child_addrs = child.find('addrs')
+        child_parts = child.find("parts")
+        child_addrs = child.find("addrs")
         if child_parts is not None:
-            for child_part in child_parts.findall('part'):
+            for child_part in child_parts.findall("part"):
                 insert_part(conn, id_mms, child_part)
         if child_addrs is not None:
-            for child_addr in child_addrs.findall('addr'):
+            for child_addr in child_addrs.findall("addr"):
                 insert_addr(conn, id_mms, child_addr)
     return id_mms
 
 
 def insert_part(conn, id_mms, child_mms):
-    columns_part = 'fk_id_mms ,'+', '.join(child_mms.attrib.keys())
-    placeholders_part = ', '.join('?' * (len(child_mms.attrib)+1))
-    sql = 'INSERT INTO parts ({}) VALUES ({})'.format(columns_part, placeholders_part)
-    vals = child_mms.attrib.values()
+    columns_part = "fk_id_mms ," + ", ".join(child_mms.attrib.keys())
+    placeholders_part = ", ".join("?" * (len(child_mms.attrib) + 1))
+    sql = "INSERT INTO parts ({}) VALUES ({})".format(columns_part, placeholders_part)
+    vals = list(child_mms.attrib.values())
     vals.insert(0, id_mms)
     return insert_default(conn, sql, vals, child_mms)
 
 
 def insert_addr(conn, id_mms, child_mms):
-    columnsAddr = 'fk_id_mms ,'+', '.join(child_mms.attrib.keys())
-    placeholdersAddr = ', '.join('?' * (len(child_mms.attrib)+1))
-    sql = 'INSERT INTO addrs ({}) VALUES ({})'.format(columnsAddr, placeholdersAddr)
-    vals = child_mms.attrib.values()
+    columnsAddr = "fk_id_mms ," + ", ".join(child_mms.attrib.keys())
+    placeholdersAddr = ", ".join("?" * (len(child_mms.attrib) + 1))
+    sql = "INSERT INTO addrs ({}) VALUES ({})".format(columnsAddr, placeholdersAddr)
+    vals = list(child_mms.attrib.values())
     vals.insert(0, id_mms)
     return insert_default(conn, sql, vals, child_mms)
 
@@ -352,7 +439,7 @@ def load_into_db(conn, tree):
     root = tree.getroot()
     log.debug("Loading MMS data into DB...")
     num_skipped = 0
-    mms_list = []
+    total_records = 0
     for child in root:
         rst = False
         if child.tag == "mms":
@@ -361,36 +448,59 @@ def load_into_db(conn, tree):
             rst = insert_sms(conn, child)
         if not rst:
             num_skipped += 1
+        else:
+            total_records += 1
 
     root.clear()  # Clear this super huge tree. We don't need it anymore
     log.debug("Done duplicate check. Skipped duplicate entries: " + str(num_skipped))
     conn.commit()
-    return mms_list
+    return total_records
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Combine XML files created with the program SMS Backup and restore.')
-    parser.add_argument('-i', '--input', metavar='infile', type=str, nargs='+',
-                        help='the input files to combine')
-    parser.add_argument('-o', '--output', metavar='outfile', type=str, nargs=1,
-                        help='the output file to write')
+    parser = argparse.ArgumentParser(
+        description="Combine XML files created with the program SMS Backup and restore."
+    )
+    parser.add_argument(
+        "-i",
+        "--input",
+        metavar="infile",
+        type=str,
+        nargs="+",
+        help="the input files to combine",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        metavar="outfile",
+        type=str,
+        nargs=1,
+        help="the output file to write",
+    )
 
     args = parser.parse_args()
 
     input_path = set()
 
     for current_path in args.input:
-        temp_path = os.path.abspath(os.path.expandvars(os.path.expanduser(current_path)))
+        temp_path = os.path.abspath(
+            os.path.expandvars(os.path.expanduser(current_path))
+        )
 
         if os.path.isdir(temp_path):
-            unfiltered_list = [os.path.normpath(os.path.join(temp_path, x)) for x in os.listdir(temp_path)]
+            unfiltered_list = [
+                os.path.normpath(os.path.join(temp_path, x))
+                for x in os.listdir(temp_path)
+            ]
         else:
             unfiltered_list = glob.glob(temp_path)
 
         filtered_list = fnmatch.filter(unfiltered_list, "*.xml")
         input_path.update(filtered_list)
 
-    return input_path, os.path.abspath(os.path.expandvars(os.path.expanduser(args.output[0])))
+    return input_path, os.path.abspath(
+        os.path.expandvars(os.path.expanduser(args.output[0]))
+    )
 
 
 if __name__ == "__main__":
